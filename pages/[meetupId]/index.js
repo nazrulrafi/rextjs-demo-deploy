47efe561a4652ad6896590722737b8878a414ae3
nazrul rafi <31164893+nazrulrafi@users.noreply.github.com>
2025-01-03 19:16:12 +0600

    updating details page

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -17,7 +17,6 @@ export async function getStaticPaths() {
         'mongodb+srv://nazrulrafi:nJhGdosohk5s2kkZ@cluster0.1cyjj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
     );
     const db = client.db();
-
     const meetupsCollection = db.collection('meetups');
 
     const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray();
@@ -25,7 +24,7 @@ export async function getStaticPaths() {
     client.close();
 
     return {
-        fallback: false,
+        fallback: 'blocking', // Make sure the page is blocked until all static pages are built
         paths: meetups.map((meetup) => ({
             params: { meetupId: meetup._id.toString() },
         })),
@@ -33,22 +32,25 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps(context) {
-    // fetch data for a single meetup
-
     const meetupId = context.params.meetupId;
 
     const client = await MongoClient.connect(
         'mongodb+srv://nazrulrafi:nJhGdosohk5s2kkZ@cluster0.1cyjj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
     );
     const db = client.db();
-
     const meetupsCollection = db.collection('meetups');
 
     const selectedMeetup = await meetupsCollection.findOne({
-        _id:new ObjectId(meetupId),
+        _id: new ObjectId(meetupId),
     });
 
     client.close();
+
+    if (!selectedMeetup) {
+        return {
+            notFound: true, // Return a 404 if the meetup is not found
+        };
+    }
 
     return {
         props: {
@@ -64,10 +66,3 @@ export async function getStaticProps(context) {
 }
 
 export default MeetupDetails;
-
-
-
-
-
-
-
